9a1a11102f135a478c47d5d4deeb8d655cc96874
Matthew Dunbar <25466545+m-dunbar@users.noreply.github.com>
2025-10-22 10:32:32 -0400

    dynamodb

--- terraform/aws/environment/dev/04.dynamodb/dynamodb.locals.tf
+++ terraform/aws/environment/dev/04.dynamodb/dynamodb.locals.tf
@@ -3,7 +3,7 @@
 #       :: mdunbar :: 2025 Oct 12 :: MIT License © 2025 Matthew Dunbar ::
 # =============================================================================
 locals {
-  kms_key_arn = try(data.aws_kms_key.terraform.arn, "KMS key not found")
+  kms_key_arn = try(data.aws_kms_key.terraform[0].arn, "KMS key not found")
 }
 
 # =============================================================================
