44d8aca9c5dfff76705513f030fc10d09161e793
Matthew Dunbar <25466545+m-dunbar@users.noreply.github.com>
2025-12-01 19:28:43 -0500

    update provider comment metadata

--- terraform/aws/environment/dev/07.vpc/provider.tf
+++ terraform/aws/environment/dev/07.vpc/provider.tf
@@ -1,5 +1,5 @@
 # =============================================================================
-# terraform_integration_templates :: environment/dev/05.s3/provider.tf 
+# terraform_integration_templates :: environment/dev/07.vpc/provider.tf 
 #      :: mdunbar :: 2025 oct 05 :: MIT License © 2025 Matthew Dunbar ::
 # =============================================================================
 terraform {
