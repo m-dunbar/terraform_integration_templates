8f41069205f013b441c000ad4fab1585b3bf2099
Matthew Dunbar <25466545+m-dunbar@users.noreply.github.com>
2025-12-01 19:19:36 -0500

    rename 01

--- terraform/aws/environment/dev/01.auth_providers/main.tf
+++ terraform/aws/environment/dev/01.auth_providers/main.tf
@@ -13,6 +13,18 @@ output "auth0_metadata_xml" {
   value = module.auth0_metadata_xml.auth0_metadata_xml
 }
 
+# resource "aws_iam_openid_connect_provider" "auth0_oidc" {
+#   url = "https://YOUR_DOMAIN.auth0.com/"
+
+#   client_id_list = [
+#     auth0_client.aws_oidc.client_id
+#   ]
+
+#   thumbprint_list = [
+#     "YOUR_AUTH0_ROOT_CA_THUMBPRINT"
+#   ]
+# }
+
 resource "aws_iam_saml_provider" "auth0_saml" {
   name                   = "auth0-saml-provider"
   saml_metadata_document = module.auth0_metadata_xml.auth0_metadata_xml
