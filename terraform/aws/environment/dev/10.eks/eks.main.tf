8e9fecaed9ce2348f51b562c769867b4d91e1122
Matthew Dunbar <25466545+m-dunbar@users.noreply.github.com>
2026-02-25 09:29:59 -0500

    Add EKS monitoring -- in progress

--- terraform/aws/environment/dev/10.eks/eks.main.tf
+++ terraform/aws/environment/dev/10.eks/eks.main.tf
@@ -2,21 +2,6 @@
 # terraform_integration_templates :: terraform/aws/environment/dev/10.eks/eks.main.tf
 #       :: mdunbar :: 2026 Feb 21 :: MIT License © 2025 Matthew Dunbar ::
 # =============================================================================
-# Supporting EKS pre-requisites
-# -------------------------------------
-# new IPAM subnets (two private, one per AZ, two public, one per AZ)
-# module "subnets_from_ipam" {
-#   source          = "../../../../modules/aws/subnets_from_ipam"
-#   vpc_id          = data.aws_vpc.dev_vpc.id
-#   ipam_pool_id    = data.aws_vpc_ipam_pool.source_pool.id
-
-#   cluster_label    = "demo"
-#   number_of_azs   = 2
-
-#   private_netmask = 26
-#   public_netmask  = 28
-# }
-
 module "eks_cluster" {
   environment   = local.environment
   cluster_label = local.cluster_label
